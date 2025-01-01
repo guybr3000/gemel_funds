8e35df81b53410937a3940b95a4ce1c344d03944
guybr3000 <guybr3000@gmail.com>
2024-12-31 22:03:11 -0600

    update for packages

--- funds.py
+++ funds.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
